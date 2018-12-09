8af4967d0cac3f07528ddbbc62bb1e8b1126b27e
Geo Cata <geo.cata@inexistent.com>
2018-12-10 00:44:15 +0200

    Merge origin/master
    Conflicts:
    	README.md

---
+++ src/components/mainView/articles/2018/ConventionsRootForEvolution.vue
@@ -0,0 +1,223 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="articleTitle">
+                    Conventions - The root for evolution
+                </h2>
+                <p class="articleDateTime" data-dateNumber="20181208">
+                    Published: Saturday, 8 December 2018
+                </p>
+                <br/><br/>
+                <p class="articleSubTitle">
+                    Why should the focus be on conventions and better practices (in this order)?
+                </p>
+                <div class="articleParagraph">
+                    There is no doubt that human imagination has no limits. We can imagine, we can abstract
+                    at a high level pretty complex concepts and ideas, but, eventually, we are limited to 
+                    what we can deploy, what we can write, all the details we can hold in our heads.
+                </div>
+                <div class="articleParagraph">
+                    If we reach that limitation with a project, development will become slower, things 
+                    will become hard to manage, to understand, things will get out of hand.
+                    In order to overcome, to some degree, this impediment, we should have good code design,
+                    a good architecture, we should make things seem  more natural.
+                </div><br/>
+                <p class="articleSubTitle">
+                    How to make things seem more natural?
+                </p>
+                <p>
+                    By following some current, accepted, good coding practices and conventions which are 
+                    proven solutions to our human limitations.
+                </p>
+                <p class="articleSubTitle">
+                    What is a convention? (2 definitions from the dictionary)
+                </p>
+                <ol>
+                    <li>"A way in which something is usually done."</li>
+                    <li>"An agreement between states covering particular matters, especially one less formal than a treaty."</li>
+                </ol>
+                <p>
+                    From this definition : "A way in which something is usually done." , we realize that 
+                    conventions will help us work together as a group on some activities because they are 
+                    an agreement, understood by many, on how things are usually done.
+                    They should be accepted as a base for carrying out some work, but they should not be 
+                    enforced (the word "usually" by definition implies something flexible). The conventions 
+                    will help us to some degree, to feel more comfortable with our code base, find our
+                    way fast when changes are required.
+                </p>
+                <p class="articleSubTitle">
+                    Why is development required, why are changes required?
+                </p>
+                <div class="articleParagraph">
+                    Preservation is in our nature as human beings. We are always seeking for new ways 
+                    of making our lives simpler. We drive a car instead of walking, we prefer to 
+                    manage machinery instead 
+                    of managing and working with people. Because of this, we have always tried to
+                    find new ways in which things "should" be done to have higher satisfaction and 
+                    preservation levels.
+                </div>
+                <div class="articleParagraph">
+                    You noticed how things changed because of human nature? From "usually done" we went to 
+                    "should be done". We found a link between conventions and development/evolution.
+                    It is in our nature to feel comfortable about things and of course software is no 
+                    exception.
+                </div><br/>
+                <p class="articleSubTitle">
+                    Rules are meant to be broken (for the good of course)
+                </p>
+                <div class="articleParagraph">
+                    Having established "A way in which something is usually done.", next we have to adapt. 
+                    As developers, we constantly have to do that. Every day, we have new problems to solve.
+                    New problems which may make us uncomfortable when following blindly the way in which
+                    something is usually done. 
+                </div>
+                <div class="articleParagraph">
+                    How to become comfortable with the new set of problems? 
+                </div>
+                <div class="articleParagraph">
+                    Establish new conventions, better practices of course.
+                </div>
+                <div class="articleParagraph">
+                    We have been doing this since forever. Look how human languages have evolved, how 
+                    many new concepts we have created that we take them for granted now, like money or 
+                    the economy. We simply shared with one another the knowledge, we agreed 
+                    that it's improving right now our way of life and accepted it as a new convention. 
+                    We all agreed for example, in the case of money, that bartering will not cut it anymore,
+                    it's too hard to work with, to keep track of the value of every good in order arrange
+                    a fair deal. We have agreed to use a single unit to describe value. We have paved the 
+                    way for something simpler, more natural and scalable.
+                </div><br/>
+                <p class="articleSubTitle">
+                    Who defines the proper conventions that we, as developers, should follow? 
+                </p>
+                <div class="articleParagraph">
+                    We do, as a community, by sharing with others our ideas that we worked so hard on and
+                    that we tried and tested and we realized that they improve development, not just by
+                    the number of problems we can solve in a given amount of time but also the way we feel
+                    about our code.
+                </div>
+                <div class="articleParagraph">
+                    This does not imply that we should follow blindly every set of rules the majority 
+                    decides. We should also strive to understand the current accepted ways of doing things 
+                    and come up with new ideas if we see any flaws or find a way to make things seem simpler,
+                    more natural. 
+                </div>
+                <div class="articleParagraph">
+                    Even if our new ideas, solutions are not accepted by the majority, part of them can 
+                    still be helpful, not just for others who can build an improved version of our solutions,
+                    but for ourselves also, it is part of our evolution as software developers, engineers 
+                    that give shape to our world.
+                </div>
+                <div class="articleParagraph">
+                    We should not focus on aligning 100% to some practices imposed by a majority, or some
+                    organization that tries to enforce them. We should strive to come up with better
+                    practices and not force every solution to be implemented in the same way. We should
+                    strive to evolve and find better ways of solving problems. We should run away from 
+                    organizations, groups that are not flexible, where teams stay aligned 100% with the 
+                    organization. These kind of groups, organizations lack the feel of evolution, and when
+                    they do decide to follow new, better practices, the costs might be too high to apply 
+                    the changes (in the video linked below, at the end after minute 60, you can see an 
+                    example where a company, Knight Capital Group, lost more than 450 million when they 
+                    decided to update legacy code, 8 years old code. Shortly after this error, they 
+                    agreed to be acquired by another company). 
+                </div>
+                <div class="articleParagraph">
+                    We should work with and follow people who want to apply better practices even if 
+                    they miss some of the requirements of a problem. They are the kind of people 
+                    who feel natural with their work, confident and can fit more details in their heads
+                    about a problem or a solution.<br/><br/>
+
+                    <img class="img-fluid mx-auto d-block" src="../../../../assets/conventionsDiagram.png" alt="" id="conventionsDiagram"><br/><br/>
+                </div>
+                <div class="articleParagraph">
+                    If a company is focused on following some practices strictly with less room for
+                    flexibility, would you want to follow such an organization? Will that company be able 
+                    to keep up with progress when its priority is to enforce some standards for better 
+                    delivery frequency? Will that company last long on a market that is constantly changing?
+                    I would answer no to all these questions. I know that companies still try to keep up 
+                    with progress, but sadly, many times is not because of their internal culture, 
+                    but because of our desire to evolve. 
+                </div>
+                <div class="articleParagraph">
+                    "Our standardized monthly scrum performance report shows your delivery rate is below
+                    average". 
+                </div>
+                <div class="articleParagraph">
+                    These types of reports generally deny the existence of technical debt, 
+                    rule out flexibility, they enforce the organization standards. 
+                </div>
+                <div class="articleParagraph">
+                    I do agree that there must be deadlines, reasonable ones, but these kind of
+                    performance reports that focus on delivery often do not take into consideration if 
+                    the code is maintainable, robust and if the ability to change comes natural. Many 
+                    problems we are trying to solve are different and often when diving in the details, 
+                    more problems emerge, problems which you may not notice if you are forced by some 
+                    organization's standards.
+                </div><br/>
+                <p class="articleSubTitle">
+                    Conclusion
+                </p>
+                <div class="articleParagraph">
+                    <b><i>Use the conventions as a root for evolution.</i></b><br/>
+                </div>
+                <div class="articleParagraph">
+                    I think we should follow proper conventions, but we should also leave 
+                    room for flexibility and strive towards evolution, natural development practices, 
+                    a more robust codebase. 
+                </div>
+                <p>
+                    The link I promised earlier. It's a very interesting presentation about simplicity
+                    in software development. You can view it <a href="https://www.youtube.com/watch?v=B3b4tremI5o" target="_blank">here</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import $ from 'jquery';
+    import hljs from 'highlight.js';
+    
+    export default {
+        mounted: function(){
+            $('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+            });
+        },
+        methods: {
+            
+        },
+        
+    };
+</script>
+
+<style>
+    @import "../../../../../node_modules/highlight.js/styles/github.css";
+</style>
+
+<style scoped>
+    
+    .articleDateTime {
+        font-style: italic;
+        color: rgb(119, 119, 119);
+    }
+    
+    .articleTitle {
+        padding-left: 20px;
+    }
+    
+    p, .articleParagraph {
+        text-indent: 20px;
+    }
+    
+    .articleSubTitle {
+        font-weight: bold;
+        font-style: italic;
+        font-size: 110%;
+    }
+    
+    #conventionsDiagram {
+            min-width: 350px;
+    }
+</style>
